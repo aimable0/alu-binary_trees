77545cf6fdc76bae416c9e80068dd8de845b987d
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-19 21:13:58 +0200

    post order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdbool.h>
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+        return;
+
+    /* handle left substree */
+    if (binary_tree_is_leaf(tree->left) == 1)
+        func(tree->left->n);
+    else
+        binary_tree_postorder(tree->left, func);
+
+    /* handle right substree */
+    if (binary_tree_is_leaf(tree->right) == 1)
+        func(tree->right->n);
+    else
+        binary_tree_postorder(tree->right, func);
+
+    /* handle middle node */
+    func(tree->n);
+}
