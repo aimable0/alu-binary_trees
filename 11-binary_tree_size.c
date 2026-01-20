3ccb76f427954173727597d70a67fe4fcad825b4
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-20 16:07:19 +0200

    BT size func

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,7 +5,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
     if (tree == NULL)
         return 0;
 
-    tot++; // parent node
+    tot++; /* parent node */
     tot += binary_tree_size(tree->left) + binary_tree_size(tree->right);
     return tot;
 }
