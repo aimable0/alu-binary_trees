8f07f1ee6040eb41855a32665bfeee2aa9744f56
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-20 15:41:31 +0200

    building independent height and depth functions

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdbool.h>
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,8 @@ int max(int a, int b)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL || binary_tree_is_leaf(tree))
+    if (tree == NULL ||
+        (tree->left == NULL && tree->right == NULL))
         return (0);
 
     return (max(binary_tree_height(tree->left),
