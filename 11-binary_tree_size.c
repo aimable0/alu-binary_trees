0abdcdc1d868f59987f40c5940d01f5d8db0fd3f
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-20 16:05:24 +0200

    binary tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{   size_t tot = 0;
+    if (tree == NULL)
+        return 0;
+
+    tot++; // parent node
+    tot += binary_tree_size(tree->left) + binary_tree_size(tree->right);
+    return tot;
+}
