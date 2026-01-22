f7ad607b0a26d5823d69a788004ce1c89d9035b8
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-22 18:38:24 +0200

    betty styles

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,11 +7,15 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-    if (node->left == NULL && node->right == NULL)
-        return (1);
-    return (0);
+if (node == NULL)
+{
+return (0);
+}
+if (node->left == NULL && node->right == NULL)
+{
+return (1);
+}
+return (0);
 }
 
 /**
@@ -21,22 +25,36 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+if (tree == NULL)
+{
+return (0);
+}
 
-    if (tree->left || tree->right)
-        if (!tree->right || !tree->left)
-            return 0;
+if (tree->left || tree->right)
+{
+if (!tree->right || !tree->left)
+{
+return (0);
+}
+}
 
-    /* check left subtree if any */
-    if (!binary_tree_is_leaf(tree->left))
-        if (!binary_tree_is_full(tree->left))
-            return 0;
+/* check left subtree if any */
+if (!binary_tree_is_leaf(tree->left))
+{
+if (!binary_tree_is_full(tree->left))
+{
+return (0);
+}
+}
 
-    /* check right substree if any */
-    if (!binary_tree_is_leaf(tree->right))
-        if (!binary_tree_is_full(tree->right))
-            return 0;
+/* check right substree if any */
+if (!binary_tree_is_leaf(tree->right))
+{
+if (!binary_tree_is_full(tree->right))
+{
+return (0);
+}
+}
 
-    return 1;
+return 1;
 }
--- sandbox.c
+++ sandbox.c
@@ -1,30 +1,42 @@
-#include <stdio.h>
-
-int main()
-{
-    int bl = 0; // 0 = false
-    if (!bl) // true
-        printf("Hello\n");
-    if (bl) // false
-        printf("World!\n");
-}
-
 #include "binary_trees.h"
-#include <stdio.h>
 
-
-size_t binary_tree_total_nodes(const binary_tree_t *tree)
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf (has no kids)
+ *
+ * Return: 1 if node is leaf and 0 otherwise.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    size_t tot = 0;
-    if (tree == NULL)
+    if (node == NULL)
         return (0);
-    tot++;
-    tot += binary_tree_total_nodes(tree->left) + binary_tree_total_nodes(tree->right);
-    return (tot);
+    if (node->left == NULL && node->right == NULL)
+        return (1);
+    return (0);
 }
 
+/**
+ * binary_tree_is_full - checks if a each node has exactly two or zero children
+ *
+ * Return: 1 if tree is full and 0 otherwise.
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int tot_nodes = binary_tree_total_nodes(tree);
-    return tot_nodes % 2 != 0 ? 1 : 0;
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left || tree->right)
+        if (!tree->right || !tree->left)
+            return (0);
+
+    /* check left subtree if any */
+    if (!binary_tree_is_leaf(tree->left))
+        if (!binary_tree_is_full(tree->left))
+            return (0);
+
+    /* check right substree if any */
+    if (!binary_tree_is_leaf(tree->right))
+        if (!binary_tree_is_full(tree->right))
+            return (0);
+
+    return 1;
 }
