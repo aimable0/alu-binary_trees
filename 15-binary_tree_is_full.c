ce5548f3e03b758fbd5242f8e728026323241f1a
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-22 18:34:04 +0200

    real implementation for checking if binary_tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,19 +1,32 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
-
-size_t binary_tree_total_nodes(const binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    size_t tot = 0;
-    if (tree == NULL)
+    if (node == NULL)
         return (0);
-    tot++;
-    tot += binary_tree_total_nodes(tree->left) + binary_tree_total_nodes(tree->right);
-    return (tot);
+    if (node->left == NULL && node->right == NULL)
+        return (1);
+    return (0);
 }
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int tot_nodes = binary_tree_total_nodes(tree);
-    return tot_nodes % 2 != 0 ? 1 : 0;
+    if (tree == NULL)
+        return 0;
+
+    if (tree->left || tree->right)
+        if (!tree->right || !tree->left)
+            return 0;
+
+    /* check left subtree if any */
+    if (!binary_tree_is_leaf(tree->left))
+        if (!binary_tree_is_full(tree->left))
+            return 0;
+
+    /* check right substree if any */
+    if (!binary_tree_is_leaf(tree->right))
+        if (!binary_tree_is_full(tree->right))
+            return 0;
+
+    return 1;
 }
