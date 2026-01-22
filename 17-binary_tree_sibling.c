6b5a56d13e4ad8b0d6fb7902e9f5b130bf556615
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-22 20:38:36 +0200

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,24 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - yields a node's sibling (if any)
+ * @node: node whose sibling is to be returned.
+ *
+ * Return: sibling node if found or NULL otherwise.
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
     if (node == NULL || node->parent == NULL)
-        return NULL;
+    {
+        return (NULL);
+    }
 
     if (node->parent->left == node)
-        return (node->parent->right == NULL) ? NULL : node->parent->right;
+    {
+        return ((node->parent->right == NULL) ? NULL : node->parent->right);
+    }
     else
-        return (node->parent->left == NULL) ? NULL : node->parent->left;
+    {
+        return ((node->parent->left == NULL) ? NULL : node->parent->left);
+    }
 }
