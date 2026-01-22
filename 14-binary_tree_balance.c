4b7539f08bb2de51bd4de0f7135c05cb5eb2da3d
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-22 23:08:46 +0200

    bt 1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,44 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
+/**
+ * binary_tree_height - calculates the binary tree height (longest path downward)
+ * @tree: the tree whose height is to be calculated.
+ *
+ * Return: height of tree (size_t) or 0
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left, right;
+size_t left, right;
 
-    if (tree == NULL)
-        return (0);
-
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
-
-    if (left > right)
-        return left + 1;
-    return right + 1;
+if (tree == NULL)
+{
+return (0);
 }
 
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+
+if (left > right)
+{
+return (left + 1);
+}
+return (right + 1);
+}
+
+/**
+ * binary_tree_balance - checks if a binary tree is balanced
+ * @tree: tree to check if balanced
+ *
+ * Return: 1 if balance and 0 otherwise.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+{
+return (0);
+}
 
-    return (binary_tree_height(tree->left) -
-            binary_tree_height(tree->right));
+return (binary_tree_height(tree->left) -
+binary_tree_height(tree->right));
 }
