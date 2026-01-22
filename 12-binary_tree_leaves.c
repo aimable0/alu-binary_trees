f8b8b98da10ca36c3dcbd69cf075dffdfa95ded7
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-01-22 23:14:41 +0200

    bt 1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - checks if a node is a leaf.
+ * @tree: node to check.
+ *
+ * Return: 1 if true or 0 otherwise.
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
 
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+return (binary_tree_leaves(tree->left)
++ binary_tree_leaves(tree->right));
 }
